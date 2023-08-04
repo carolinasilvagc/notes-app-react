f605b01840d747bcc6038c055d249c1f62d8b471
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-04 16:08:24 -0300

    Finalizando a página de Edição de Nota

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,10 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {IoIosArrowBack} from 'react-icons/io'
+import {RiDeleteBin6Line} from 'react-icons/ri'
 
 const EditNote = () => {
   return (
-    <div>
-      EditNote
-    </div>
+    <section>
+      <header className='create-note__header'>
+        <Link to='/' className='btn'><IoIosArrowBack/></Link>
+        <button className="btn lg primary">Salvar</button>
+        <button className="btn danger"><RiDeleteBin6Line /></button>
+      </header>
+      <form className='create-note__form'>
+        <input type='text' autoFocus placeholder='Título' />
+        <textarea placeholder='Digite sua nota...' rows='28'></textarea>
+      </form>
+    </section>
   )
 }
 
