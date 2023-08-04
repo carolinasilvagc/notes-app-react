245fd85e11206ffccb92ac809243b44b2e461188
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-04 15:59:28 -0300

    Finalizando a página de Notes, inicial da aplicação.

--- src/pages/Notes.jsx
+++ src/pages/Notes.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+import React from 'react';
+import {CiSearch} from 'react-icons/ci';
+import dummyNotes from '../dummy_notes';
+import { Link } from 'react-router-dom';
+import {BsPlusLg} from 'react-icons/bs';
+import NoteItem from '../components/NoteItem';
 
 const Notes = () => {
   return (
-    <div>
-      Notes
-    </div>
+    <section>
+      <header className='notes__header'>
+        <h2>Minhas Notas</h2>
+        {/* <input type='text' autoFocus placeholder='Palavra-chave...' /> */}
+        <button className='btn'> <CiSearch /></button>
+      </header>
+      <div className='notes__container'>
+        {
+          dummyNotes.map(note => <NoteItem key={note.id} note={note}/>)
+        }
+      </div>
+      <Link className='btn add__btn'><BsPlusLg/></Link>
+    </section>
   )
 }
 
