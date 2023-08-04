4419e4609319e124f9c2827fa1e0959ba3b3a91b
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-04 14:57:10 -0300

    Estruturação inicial de pastas e arquivos

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Notes from './pages/Notes';
+import CreateNote from './pages/CreateNote';
+import EditNote from './pages/EditNote';
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Notes />} />
+        <Route path="/create-note" element={<CreateNote />} />
+        <Route path="/edit-note/:id" element={<EditNote />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App
