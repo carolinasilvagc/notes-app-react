7ae86839f14ee63f358cf3fd4090f57e52aee201
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-04 16:05:32 -0300

    Finalizando a página de Criação de Nota

--- src/pages/CreateNote.jsx
+++ src/pages/CreateNote.jsx
@@ -1,10 +1,19 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import {IoIosArrowBack} from 'react-icons/io'
 
 const CreateNote = () => {
   return (
-    <div>
-      CreateNote
-    </div>
+    <section>
+      <header className='create-note__header'>
+        <Link to='/' className='btn'><IoIosArrowBack/></Link>
+        <button className="btn lg primary">Salvar</button>
+      </header>
+      <form className='create-note__form'>
+        <input type='text' autoFocus placeholder='Título' />
+        <textarea placeholder='Digite sua nota...' rows='28'></textarea>
+      </form>
+    </section>
   )
 }
 
