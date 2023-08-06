5187c637a65119aa534679d5283188433419c038
Carolina Silva <carolinasilvagc08@gmail.com>
2023-08-06 15:00:43 -0300

    Finalizando a página de Editar nota com sua funcionalidade

--- src/pages/EditNote.jsx
+++ src/pages/EditNote.jsx
@@ -1,19 +1,47 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
+import { Link, useParams, useNavigate } from 'react-router-dom'
 import {IoIosArrowBack} from 'react-icons/io'
 import {RiDeleteBin6Line} from 'react-icons/ri'
+import { useState } from 'react'
+import useCreateDate from '../components/useCreateDate'
 
-const EditNote = () => {
+const EditNote = ({notes, setNotes}) => {
+  const {id} = useParams();
+  const note = notes.find((item) => item.id === id);
+  const [title, setTitle] = useState(note.title);
+  const [details, setDetails] = useState(note.details);
+  const date = useCreateDate();
+  const navigate = useNavigate();
+
+  const handleForm = (e) => {
+    e.preventDefault();
+
+    if(title && details) {
+      const newNote = {...note, title, details, date};
+      const newNotesArray = notes.map((item) => {
+        if(item.id === id) {
+          item = newNote;
+        }
+        return item;
+      });
+
+      setNotes(newNotesArray);
+    }
+
+    // Redirect to home page
+    navigate('/');
+  }
+
   return (
     <section>
       <header className='create-note__header'>
         <Link to='/' className='btn'><IoIosArrowBack/></Link>
-        <button className="btn lg primary">Salvar</button>
+        <button className="btn lg primary" onClick={handleForm}>Salvar</button>
         <button className="btn danger"><RiDeleteBin6Line /></button>
       </header>
-      <form className='create-note__form'>
-        <input type='text' autoFocus placeholder='Título' />
-        <textarea placeholder='Digite sua nota...' rows='28'></textarea>
+      <form className='create-note__form' onSubmit={handleForm}>
+        <input type='text' autoFocus placeholder='Título' value={title} onChange={(e) => setTitle(e.target.value)}/>
+        <textarea placeholder='Digite sua nota...' rows='28' value={details} onChange={(e) => setDetails(e.target.value)}></textarea>
       </form>
     </section>
   )
